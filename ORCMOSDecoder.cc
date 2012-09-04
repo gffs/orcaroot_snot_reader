5c4e8252fba277d51913529c30e0906fabb3d6de
gffs <gaffus@gmail.com>
2012-09-04 09:36:02 -0700

    updated CMOS rate packet
    CMOS counts are shipped as received from XL3
    added timestamp, JSON, documentation

--- ORCMOSDecoder.cc
+++ ORCMOSDecoder.cc
@@ -1,2 +1,45 @@
 #include "ORCMOSDecoder.hh"
 
+#include <string>
+#include <sstream>
+
+void ORCMOSDecoder::Swap(UInt_t* dataRecord)
+{
+    //do not swap the string at the end
+    for (size_t idx = 1; idx <  21+8*32; idx++) {
+        ORUtils::Swap(dataRecord[idx]);
+    }
+}
+
+const std::string ORCMOSDecoder::ToJson(UInt_t* record)
+{
+    std::ostringstream json("{\n");
+
+    json << "\"type\":\"cmos_counts\",\n"
+        << "\"timestamp\": \"" << TimeStamp(record) << "\",\n"
+        << "\"crate_num\": " << CrateNum(record) << ",\n"
+        << "\"slot_mask\": " << SlotMask(record) << ",\n"
+        << "\"channel_mask\": [ ";
+
+    for (unsigned char i = 0; i < 16; i++) {
+        if (i) json << ", ";
+        json << ChannelMaskForSlot(i, record);
+    }
+    json << "],\n"
+        << "\"error_flags\": " << ErrorFlags(record) << ",\n"
+        << "\"counts\": [ ";
+
+    for (unsigned char slot = 0; slot < 8; slot++) {
+        if (slot) json << ",\n";
+        json << "[ ";
+        for (unsigned char channel = 0; channel < 32; channel++) {
+            if (channel) json << ", ";
+            json << CMOSCounterForSlotAndChannel(slot, channel, record);
+        }
+        json << " ]";
+    }
+    json << "]\n";
+    json << "}";
+    return json.str();
+}
+
--- ORCMOSDecoder.hh
+++ ORCMOSDecoder.hh
@@ -4,6 +4,7 @@
 #define _ORCMOSDecoder_hh_
 
 #include "ORVDataDecoder.hh"
+#include <string>
 
 class ORCMOSDecoder : public ORVDataDecoder
 {
@@ -11,35 +12,41 @@ class ORCMOSDecoder : public ORVDataDecoder
     ORCMOSDecoder() {}
     virtual ~ORCMOSDecoder() {}
 
-	virtual inline UInt_t CrateOf(UInt_t* record)
-		{ return record[1]; }
+    virtual inline UInt_t CrateOf(UInt_t* record)
+        { return record[1]; }
 
-	virtual inline UInt_t CardOf(UInt_t* /*record*/)
-		{ return 0; }
+    virtual inline UInt_t CardOf(UInt_t* /*record*/)
+        { return 0; }
 
-	virtual inline UInt_t CrateNum(UInt_t* record)
-		{ return record[1]; }
+    virtual inline UInt_t CrateNum(UInt_t* record)
+        { return record[1]; }
 
-	virtual inline UInt_t SlotMask(UInt_t* record)
-		{ return record[2]; }
+    virtual inline UInt_t SlotMask(UInt_t* record)
+        { return record[2]; }
 
-	virtual inline UInt_t ChannelMaskForSlot(UInt_t slot, UInt_t* record)
-		{ return record[3 + slot]; }
+    virtual inline UInt_t ChannelMaskForSlot(UInt_t slot, UInt_t* record)
+        { return record[3 + slot]; }
 
-	virtual inline UInt_t Delay(UInt_t* record)
-		{ return record[19]; }
-	
-	virtual inline UInt_t ErrorFlags(UInt_t* record)
-		{ return record[20]; }
+    virtual inline UInt_t Delay(UInt_t* record)
+        { return record[19]; }
+    
+    virtual inline UInt_t ErrorFlags(UInt_t* record)
+        { return record[20]; }
 
-	virtual inline Float_t CMOSRateForSlotAndChannel(UInt_t slot, UInt_t channel, UInt_t* record)
-		{ return record[21 + slot*32 + channel]; }
+    virtual inline UInt_t CMOSCounterForSlotAndChannel(UInt_t slot, UInt_t channel, UInt_t* record)
+        { return record[21 + slot*32 + channel]; }
 
-	virtual inline Float_t CMOSRateForOffset(UInt_t offset, UInt_t* record)
-		{ return record[21 + offset]; }
+    virtual inline UInt_t CMOSCounterForOffset(UInt_t offset, UInt_t* record)
+        { return record[21 + offset]; }
 
-	virtual std::string GetDataObjectPath()
-		{ return "ORXL3Model:Xl3CmosRate"; }
+    virtual const char* TimeStamp(UInt_t* record)
+        { return (const char*) (record + 21 + 8*32); }
+
+    virtual void Swap(UInt_t* dataRecord);
+    virtual const std::string ToJson(UInt_t* record);
+
+    virtual std::string GetDataObjectPath()
+        { return "ORXL3Model:Xl3CmosRate"; }
 };
 
 #endif
